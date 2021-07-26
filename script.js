a0abe285982db583e0ac2c0bf181f4fbf7531d2f
Andrew Smith <meuzishun@gmail.com>
2021-07-26 08:17:27 -0400

    Add events module

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+const events = (function() {
+    const _events = {};
+
+    function on(eventName, fn) {
+        _events[eventName] = _events[eventName] || [];
+        _events[eventName].push(fn);
+    };
+
+    function off(eventName, fn) {
+        if (_events[eventName]) {
+            for (let i = 0; i < _events[eventName].length; i++) {
+                if (_events[eventName][i] === fn) {
+                    _events[eventName].splice(i, 1);
+                    break;
+                }
+            }
+        }
+    };
+
+    function emit(eventName, data) {
+        if (_events[eventName]) {
+            _events[eventName].forEach(fn => fn(data));
+        }
+    };
+
+    return {
+        on,
+        off,
+        emit
+    };
+
+})();
+
 const gameboard = (function() {
     const grid = document.querySelector('.gameboard');
     const cells = grid.querySelectorAll('.cell');
@@ -19,11 +52,12 @@ const gameboard = (function() {
         if (XsOs[index]) { return };
         XsOs[index] = letter;
         _render();
+        events.emit('playPlayed', XsOs);
     }
     
     function clearBoard() {
         XsOs.fill('');
-        console.table(XsOs);
+        plays = 0;
         _render();
     }
     
@@ -33,8 +67,14 @@ const gameboard = (function() {
         addLetter,
         clearBoard
     };
+
 })();
 
 const gameplay = (function() {
+    function displayState(data) {
+        console.table(data);
+    }
 
+    events.on('playPlayed', displayState);
 })();
+
