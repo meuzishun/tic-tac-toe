fa6b1f38f8d298099c69f662a7e929fb5a497eff
Andrew Smith <meuzishun@gmail.com>
2021-07-26 11:47:42 -0400

    Add some gamestate functionality

--- script.js
+++ script.js
@@ -59,9 +59,19 @@ const gameboard = (function() {
         XsOs.fill('');
         plays = 0;
         _render();
+        grid.addEventListener('click', addLetter);
     }
     
-    cells.forEach(cell => cell.addEventListener('click', addLetter.bind(this)));
+    grid.addEventListener('click', addLetter);
+
+    events.on('winnerFound', (combo) => {
+        console.log(combo);
+    });
+    
+    events.on('gameOver', () => {
+        console.log('game over');
+        grid.removeEventListener('click', addLetter);
+    });
     
     return {
         addLetter,
@@ -71,10 +81,52 @@ const gameboard = (function() {
 })();
 
 const gameplay = (function() {
-    function displayState(data) {
-        console.table(data);
+    function checkGameState(data) {
+        checkBoardFilled(data);
+        checkForWinner(data);
     }
 
-    events.on('playPlayed', displayState);
+    function checkBoardFilled(data) {
+        if (data.every(item => item !== '')) {
+            events.emit('gameOver', data);
+        }
+    }
+
+    function checkForWinner(data) {
+        const winningCombos = {
+            topRow: [data[0], data[1], data[2]],
+            middleRow: [data[3], data[4], data[5]],
+            bottumRow: [data[6], data[7], data[8]],
+            leftColumn: [data[0], data[3], data[6]],
+            centerColumn: [data[1], data[4], data[7]],
+            rightColumn: [data[2], data[5], data[8]],
+            leftRightDiagonal: [data[0], data[4], data[8]],
+            rightleftDiagonal: [data[2], data[4], data[6]]
+        }
+
+        for (const combo in winningCombos) {
+            if (winningCombos[combo].every((item, index, array) => item !== '' && item === array[0])) {
+                events.emit('winnerFound', combo);
+                events.emit('gameOver', data);
+            }
+            // if (winningCombos[combo].every(item => item === 'O')) {
+            //     events.emit('winnerFound', winningCombos[combo]);
+            // }
+        }
+
+        // const topRow = [data[0], data[1], data[2]];
+        // const middleRow = [data[3], data[4], data[5]];
+        // const bottumRow = [data[6], data[7], data[8]];
+        // const leftColumn = [data[0], data[3], data[6]];
+        // const centerColumn = [data[1], data[4], data[7]];
+        // const rightColumn = [data[2], data[5], data[8]];
+        // const leftRightDiagonal = [data[0], data[4], data[8]];
+        // const rightleftDiagonal = [data[2], data[4], data[6]];
+
+
+    }
+    
+    events.on('playPlayed', checkGameState);
+
 })();
 
