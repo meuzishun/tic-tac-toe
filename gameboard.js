ef217fa6a69fc6d1d6af89c68c1295047233c95e
Andrew Smith <meuzishun@gmail.com>
2021-07-28 14:12:14 -0400

    Add messages and results modules

--- gameboard.js
+++ gameboard.js
@@ -1,10 +1,8 @@
 const gameboard = (function() {
-    const gameContainer = document.querySelector('.game-container')
+    const gameContainer = document.querySelector('.game-container');
     const gameboard = gameContainer.querySelector('.gameboard');
     const cells = gameboard.querySelectorAll('.cell');
-    const lines = gameboard.querySelectorAll('.row, .column, .diagonal');
     const newGameBtn = gameContainer.querySelector('.newGame-btn');
-    const messageDisplay = gameContainer.querySelector('.message-display');
     
     function handleCellClick(evt) {
         let cellIndex = [...cells].indexOf(evt.target);
@@ -16,35 +14,9 @@ const gameboard = (function() {
             cells[index].textContent = entry;
         });
     }
-
-    function showWinningLine(lineName) {
-        lines.forEach(line => {
-            if (line.classList.contains(lineName)) {
-                line.classList.remove('hide');
-            }
-        });
-    }
-
-    function hideAllLines() {
-        lines.forEach(line => {
-            if (!line.classList.contains('hide')) {
-                line.classList.add('hide');
-            }
-        });
-    }
-
-    function declareWinner(winner) {
-        if (!winner) {
-            messageDisplay.textContent = `It's a draw`;
-        } else {
-            messageDisplay.textContent = `${winner} wins!`;
-        }
-    }
     
     function clearBoard() {
         events.emit('clearBoard', null);
-        hideAllLines();
-        messageDisplay.textContent = '';
     }
 
     function endGame() {
@@ -62,8 +34,6 @@ const gameboard = (function() {
     newGameBtn.addEventListener('click', newGame);
 
     events.on('boardDataChanged', mapDataToBoard);
-    events.on('rowOfThree', showWinningLine)
-    events.on('winnerFound', declareWinner);
     events.on('gameOver', endGame);
 
 })();
