b6aa41d6bbe6dc69e094270cf2c0e141e7d584c0
Andrew Smith <meuzishun@gmail.com>
2021-08-04 14:29:53 -0400

    rename name inputs

--- js/input.js
+++ js/input.js
@@ -1,23 +1,23 @@
 (function() {
     const setupContainer = gameContainer.querySelector('.player-setup-container');
-    const playerNameInputs = [...setupContainer.querySelectorAll('.player-name-input')];
+    const nameInputs = [...setupContainer.querySelectorAll('.player-name-input')];
 
-    playerNameInputs.forEach(input => input.addEventListener('input', validateNameInputs));
+    nameInputs.forEach(input => input.addEventListener('input', validateNameInputs));
 
     function validateNameInputs() {
-        if (playerNameInputs.every(input => input.value !== '')) {
+        if (nameInputs.every(input => input.value !== '')) {
             events.emit('namesEntered', null);
         }
     }
 
     function sendNames() {
-        let names = playerNameInputs.map(input => input.value);
+        let names = nameInputs.map(input => input.value);
         names.forEach((name, index) => {
             const markers = ['X', 'O'];
             let player = createPlayer(name, markers[index]);
             events.emit('newPlayer', player);
         });
-        playerNameInputs.forEach(input => input.value = '');
+        nameInputs.forEach(input => input.value = '');
         setupContainer.classList.add('hide-container');
     }
 
