f3a85749624ab039b3c3e38dce90be955d1ee57a
Andrew Smith <meuzishun@gmail.com>
2021-08-09 16:39:35 -0400

    Clean up messages module

--- js/messages.js
+++ js/messages.js
@@ -1,7 +1,15 @@
 (function() {
     const messageDisplay = document.querySelector('.message');
 
-    function formatMessage(winner) {
+    function formatMessage(winnerName) { 
+        return winnerName ? `${winnerName} wins!` : `It's a draw`;
+    }
+
+    function displayMessage(message) {
+        messageDisplay.textContent = message;
+    }
+
+    function declareWinner(winner) {
         let winnerName;
         let playerType;
         if (winner) {
@@ -13,20 +21,7 @@
         if (playerType === 'computer') {
             winnerName = winner.getMarker();
         }
-            
-        return winnerName ? `${winnerName} wins!` : `It's a draw`;
-    }
-
-    function displayMessage(message) {
-        messageDisplay.textContent = message;
-    }
-
-    // function declareWinner(winnerName) {
-    //     displayMessage(formatMessage(winnerName));
-    // }
-
-    function declareWinner(winner) {
-        displayMessage(formatMessage(winner));
+        displayMessage(formatMessage(winnerName));
     }
 
     function clearMessage() {
